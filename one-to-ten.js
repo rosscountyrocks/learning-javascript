34a41318533bb88547be663d5b833105d399e6a2
Lawrence Berry <lawrence.berry@gmail.com>
2014-10-25 21:53:38 +0100

    Some javascript involving the numbers from one to ten

---
+++ one-to-ten.js
@@ -0,0 +1,68 @@
+console.log(3)
+console.log("hello Jayden")
+hello Jayden
+
+for(i = 0; i < 23; i++){
+console.log("22")
+}
+
+for(i = 0; i < 10; i++){
+console.log("22")
+}
+
+for(i = 0; i < 10; i++){
+console.log(i)
+}
+0
+1
+2
+3
+4
+5
+6
+7
+8
+9
+undefined
+for(i = 0; i < 11; i++){
+console.log(i)
+}
+0
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+undefined
+var total = 0;
+undefined
+for(i = 0; i < 10; i++){
+total = total + i;
+}
+45
+["zero", "one", "two", "3", "4", "5", "6", "7","8", "9", "ten"]
+["zero", "one", "two", "3", "4", "5", "6", "7", "8", "9", "ten"]
+var = numbers = ["zero", "one", "two", "3", "4", "5", "6", "7","8", "9", "ten"]
+Uncaught SyntaxError: Unexpected token = VM2128:732
+var numbers = ["zero", "one", "two", "3", "4", "5", "6", "7","8", "9", "ten"]
+undefined
+for(i = 0; i < 11; i++){
+console.log(numbers[i])
+}
+zero
+one
+two
+3
+4
+5
+6
+7
+8
+9
+ten
+undefined
